801bc724696850244085a6374dd285803a5e0dc1
Gilberto Ocampo <ocampo@gate.net>
2020-02-19 23:33:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
-def oxford_comma(array)
+# require pry
 
+def oxford_comma(array)
+  array.join(" , ")
+end
+
+def oxford_comma(string1)
+  string1.join(" and ")
+end
+
+def oxford_comma(array2)
+  array2.size = num_items
+  counter = 0 
+  while counter < (#{num_items - 2})
+    array2(counter) << ", "
+    counter +=1 
+  end
+  array2(counter) << " and "
+  array2.join
 end
